60ec748c3b1018c71a3cdadb08df125ce01ae8c4
ccnuu <cy.arctique@gmail.com>
2026-09-08 17:46:43 +0800

    basic config

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # nvim
 
-neovim config
+Neovim config
 
+## Neovim 配置文件的位置
+
++ Unix: `~/.config/nvim/init.lua`
++ Windows: `~/AppData/Local/nvim/init.lua` （~ 一般是 C:/Users/<user>）
+
+如果找不到，在 nvim 中输入命令 `:= vim.fn.stdpath("config")`
---
+++ test/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+	std::cout << "hello world" << std::endl;
+	std::cout << "hello world" << std::endl;
+	std::cout << "hello world" << std::endl;
+	std::cout << "damn bro\n"; 
+
+	std::cout << "hello world" << std::endl;
+	std::cout << "hello world" << std::endl;
+	std::cout << "hello world" << std::endl;
+	std::cout << "damn bro\n"; 
+
+	// hello world
+	// hello world
+
+	const char* hello_world = "hello world";
+
+	return 0;
+}
